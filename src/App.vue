18095f692d365e864346420456814b6fb1d8e7e5
Alexandra <bguevaraale@gmail.com>
2022-04-25 20:35:45 -0300

    feat: adding routes, header and footer

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,37 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <NavbarComponent logo="/img/logo.png" alt='Make Your Burger'/>
   <router-view/>
+  <FooterComponent />
 </template>
 
+<script>
+import NavbarComponent from './components/Navbar.vue';
+import FooterComponent from './components/Footer.vue';
+
+export default {
+  components: {
+    NavbarComponent,
+    FooterComponent,
+  },
+
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  *{
+    font-family: Arial, Helvetica, sans-serif;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  .main-container {
+    margin: 50px;
+    min-height: 250px;
+  }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+  h1{
+    text-align: center;
+    font-size: 42px;
+    margin-bottom: #222;
+  }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <footer id="footer">
+    <p>Make your Burguer &copy; 2021</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'FooterComponent',
+};
+</script>
+
+<style scoped>
+  #footer {
+    align-items: center;
+    background-color: #222;
+    border-top: 4px solid #111;
+    bottom: 0;
+    color: #FCBA03;
+    display: flex;
+    height: 200px;
+    justify-content: center;
+    position: absolute;
+    width: 100%;
+  }
+</style>
